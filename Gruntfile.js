0d27180894bdfb46cd4e11c40ab1f2574f50916d
Asela Illayapparachchi <asela38@gmail.com>
2014-11-23 21:24:32 +0530

    Include the grunt concatenation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,26 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
     hello : {
       message: 'Hello, World!'
+    },
+
+    dirs : {
+      src: 'jsSrc',
+      dest: 'js',
+    },
+    concat: {
+      options: {
+        separator: ';\n',
+        stripBanners: false,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        src: ['<%= dirs.src %>/**.js'],
+        dest: '<%= dirs.dest %>/app.js'
+      },
     }
 
   });
@@ -15,6 +33,8 @@ module.exports = function(grunt) {
      grunt.log.ok('from: ' + grunt.config.get('pkg').name);
   });
 
-  grunt.registerTask('default', ['hello']);
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat']);
 
 };
